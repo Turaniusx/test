0920d9ba74bf14ab64a8a0f15f308dfd011deb34
turanius <dimitarturanov@gmail.com>
2022-07-29 11:21:17 +0200

    added factorial input executer

--- scripts.js
+++ scripts.js
@@ -1,8 +1,29 @@
 let num1 = document.getElementById("display-el")
+let num2 = document.getElementById("calculated-el")
+
 let count = 0
 
 function increment() {
     count++
     num1.textContent = count
     console.log(count)
+}
+function decrement() {
+    count--
+    num1.textContent = count
+    console.log(count)
+}
+function execute() {
+    var n = document.getElementById("num").value;
+    var i, f=1;
+    if (n < 0) {
+        document.getElementById("fact").innerHTML = "Error!"
+    } else {
+
+    
+    for(i=1; i<=n; i++){
+        f *= i;
+    }
+    document.getElementById("fact").innerHTML = f;
+}
 }
